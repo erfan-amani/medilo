dc5a7e1f0aa95584c1d75c1d4fe4a6610d9eee61
erfan-amani <erfan80amani23@gmail.com>
2021-08-18 14:49:33 +0430

    config firebase app and firebase auth in project

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDw-hT0fUX6ogHVQuVwSDHnsFtFwr80TnM',
+  authDomain: 'medilo.firebaseapp.com',
+  projectId: 'medilo',
+  storageBucket: 'medilo.appspot.com',
+  messagingSenderId: '605656702690',
+  appId: '1:605656702690:web:f39c007c43513d587948bb',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseApp.auth();
