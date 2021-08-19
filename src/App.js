991bf20791d1fd9c2313c80316ff973860d788b1
erfan-amani <erfan80amani23@gmail.com>
2021-08-19 17:37:29 +0430

    edited routing - added posts page and fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Card from './features/Ui/Card';
 import Signin from './features/auth/Signin/Signin';
 import Signup from './features/auth/Signup/Signup';
 import SigninWithEmail from './features/auth/Signin/SigninWithEmail';
 import SignupWithEmail from './features/auth/Signup/SignupWithEmail';
+import Posts from './features/posts/Posts';
 import { auth } from './firebase';
 import { userSignedin, userSignedout } from './features/auth/auth-slice';
-import { useDispatch } from 'react-redux';
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
 
   // add auth listener to the app
   useEffect(() => {
@@ -44,20 +46,41 @@ function App() {
     <BrowserRouter>
       <div className="flex items-center justify-center w-screen h-screen bg-gray-200">
         <Switch>
-          <Card>
+          {!user && (
             <Route path="/signup" exact>
-              <Signup />
+              <Card>
+                <Signup />
+              </Card>
             </Route>
+          )}
+          {!user && (
             <Route path="/signin" exact>
-              <Signin />
+              <Card>
+                <Signin />
+              </Card>
             </Route>
+          )}
+          {!user && (
             <Route path="/signin/email">
-              <SigninWithEmail />
+              <Card>
+                <SigninWithEmail />
+              </Card>
             </Route>
+          )}
+          {!user && (
             <Route path="/signup/email">
-              <SignupWithEmail />
+              <Card>
+                <SignupWithEmail />
+              </Card>
             </Route>
-          </Card>
+          )}
+
+          <Route path="/posts">
+            <Posts />
+          </Route>
+          <Route path="*">
+            <Redirect to="/posts" />
+          </Route>
         </Switch>
       </div>
     </BrowserRouter>
