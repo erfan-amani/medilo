143713b5dd89c4514b238d23bd55967ce982227b
erfan-amani <erfan80amani23@gmail.com>
2021-08-19 19:54:28 +0430

    created storage & firestore instance & exported

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDw-hT0fUX6ogHVQuVwSDHnsFtFwr80TnM',
@@ -12,6 +11,8 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+export const db = firebaseApp.firestore();
+export const storage = firebaseApp.storage();
 export const auth = firebaseApp.auth();
 
 export const signinWithGoogle = () => {
