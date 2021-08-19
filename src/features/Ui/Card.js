fa3ec2a8ef1fe35baf52bddf673f961303147672
erfan-amani <erfan80amani23@gmail.com>
2021-08-19 17:42:16 +0430

    added box shadow and border radius to card

--- src/features/Ui/Card.js
+++ src/features/Ui/Card.js
@@ -1,6 +1,6 @@
 const Card = (props) => {
   return (
-    <div className="flex flex-col items-center bg-white gap-4 p-12 w-11/12 sm:w-3/5 md:w-1/2 xl:w-1/3">
+    <div className="flex flex-col items-center bg-white gap-4 p-12 w-11/12 sm:w-3/5 md:w-1/2 xl:w-1/3 rounded-sm shadow-lg">
       {props.children}
     </div>
   );
