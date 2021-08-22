71e9a0a29c79301ff8bca99c3f4e286b1b06c0fc
erfan-amani <erfan80amani23@gmail.com>
2021-08-22 12:07:10 +0430

    fixed a bug - caused never changing content

--- src/features/Ui/posts/PostListSmall.js
+++ src/features/Ui/posts/PostListSmall.js
@@ -6,11 +6,13 @@ const PostListSmall = ({ posts }) => {
   );
 
   if (posts && posts.length > 0) {
-    <div className="grid grid-cols-3 lg:grid-cols-4 pb-14 md:pb-0">
-      {posts.map((post) => (
-        <PostItemSmall key={post.id} image={post.image} />
-      ))}
-    </div>;
+    content = (
+      <div className="grid grid-cols-3 lg:grid-cols-4 pb-14 md:pb-0">
+        {posts.map((post) => (
+          <PostItemSmall key={post.id} image={post.image} />
+        ))}
+      </div>
+    );
   }
 
   return content;
